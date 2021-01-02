562327ff6c28cd4b047cf2b079954a77c24fb102
fardisaz <amoozadeh73@yahoo.com>
2021-01-02 08:49:54 +0100

    complex-useState

--- src/index.js
+++ src/index.js
@@ -1,15 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const NoteApp = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const addNote = (e) => {
+    e.preventDefault();
+    setNotes([
+      ...notes,
+      {
+        title,
+        body,
+      },
+    ]);
+    setTitle("");
+    setBody("");
+  };
+  const removeNote = (title) => {
+    setNotes(notes.filter((note) => note.title !== title));
+  };
+  return (
+    <div>
+      <h1>Notes</h1>
+      {notes.map((note) => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <button onClick={() => removeNote(note.title)}>x</button>
+        </div>
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)}></input>
+        <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <button>add note</button>
+      </form>
+    </div>
+  );
+};
+
+// const App = (props) => {
+
+//   const [count, setCount] = useState(props.count);
+//   // here we showed how we can use useState multiple times to track multiple things.
+//   const [text, setText] = useState("");
+
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   return (
+//     <div>
+//       <p>
+//         The current {text || "count"} is {count}
+//       </p>
+//       <button onClick={increment}>+1</button>
+//       <button onClick={() => setCount(count - 1)}>-1</button>
+//       <button onClick={() => setCount(0)}>reset</button>
+//       <input value={text} onChange={(e) => setText(e.target.value)} />
+//     </div>
+//   );
+// };
+
+// App.defaultProps = {
+//   count: 0,
+// };
+
+ReactDOM.render(<NoteApp />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
